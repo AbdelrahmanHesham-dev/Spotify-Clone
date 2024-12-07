fba3f14fb56ad2fa66b30d53731939c8ecd66ac9
Abdelrahman <abdelrahmanhesham477@gmail.com>
2024-12-07 16:04:24 +0200

    ubdate

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/search',
       name: 'home',
       component: HomeView,
     },
     {
-      path: '/search',
+      path: '/',
       name: 'search',
       component: SearchView,
     },
